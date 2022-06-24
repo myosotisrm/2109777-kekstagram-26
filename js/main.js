d18643bca8aa403d814d271c564d9350a1e1fe4f
myosotisrm <106912630+myosotisrm@users.noreply.github.com>
2022-06-25 00:59:38 +0300

    Выполнил ДЗ2мод

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-
 function getRandomIntInclusive(min, max) {
-    if (max > min && min >= 0) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-    }
-    return console.log('Не пройдет, используй другие числа');
+  if (max > min && min >= 0) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  return 'Используй другие числа';
 }
-getRandomIntInclusive();
 
-function countSimbols () {
-  console.log(''.length)
+console.log(getRandomIntInclusive(2, 4));
+
+function countSimbols (message, maxSimbols) {
+if (message.length >= maxSimbols) {
+  return false;
+} return true;
 }
+
+console.log(countSimbols ('1234', 10))
 
 // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
+// Другой источник: Моя подруга
