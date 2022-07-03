2c74c4177f8eb9964b4d5962e5ff2cbfb77bb382
myosotisrm <106912630+myosotisrm@users.noreply.github.com>
2022-07-03 19:59:59 +0300

    Генераторы

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const createPost = () => {
+  const randomNameIndex = getRandomPositiveInteger(0, NAMES.length - 1);
+
+
+
+  return {
+    id: ,
+    avatar: '',
+    massage: '',
+    name: NAMES[randomNameIndex],
+  };
+}
