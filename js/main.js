a831733ccac088505f29b04cbe824424d268f20c
myosotisrm <106912630+myosotisrm@users.noreply.github.com>
2022-07-05 16:09:59 +0300

    Доделал

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-function getRandomPositiveInteger (a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+    function getRandomPositiveInteger (startNumber, endNumber) {
+  const lower = Math.ceil(Math.min(Math.abs(startNumber), Math.abs(endNumber)));
+  const upper = Math.floor(Math.max(Math.abs(startNumber), Math.abs(endNumber)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
@@ -16,7 +17,7 @@ const NAMES = [
   'Вашингтон',
 ];
 
-const Desc = [
+const DESCS = [
   'Люблю фоточки',
   'Хочу на море',
   'Котики - моя жизнь',
@@ -27,41 +28,42 @@ const Desc = [
   'Жизнь продолжается',
 ];
 
-const Messages = [
+const MESSAGES = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const Posts= 25;
+const MAXS_POSTS = 25;
 
-const arrayId = [];
+const commentIds = [];
 
 const createPost = () => {
   const randomNameIndex = getRandomPositiveInteger(0, NAMES.length - 1);
-  const randomID = getRandomPositiveInteger(1, Posts);
-  const i = getRandomPositiveInteger(1, Posts);
-  const randomURL = `photos/` + i + `.jpg`;
-  const randomDescIndex = getRandomPositiveInteger(0, Desc.length - 1);
+  const randomId = getRandomPositiveInteger(1, MAXS_POSTS);
+  const randomUrlNumber = getRandomPositiveInteger(1, MAXS_POSTS);
+  const randomUrl = `photos/${randomUrlNumber}.jpg`;
+  const randomDescIndex = getRandomPositiveInteger(0, DESCS.length - 1);
   const randomLikes = getRandomPositiveInteger(15, 200);
-  const createComments = () => {
-    const CommentId = arrayId.length + 1;
-    arrayId.push(1);
-    const v = getRandomPositiveInteger(1, 6);
-    const randomAvatar = `img/avatar-` + v + `.svg`;
-    const randomMessIndex = getRandomPositiveInteger(0, Messages.length - 1);
+  const createComment = () => {
+    const commentId = commentIds.length + 1;
+    commentIds.push(1);
+    const randomAvatarNumber = getRandomPositiveInteger(1, 6);
+    const randomAvatar = `img/avatar-${randomAvatarNumber}.svg`;
+    const randomMessIndex = getRandomPositiveInteger(0, MESSAGES.length - 1);
     return {
-      id: CommentId,
+      id: commentId,
       avatar: randomAvatar,
-      message: Messages[randomMessIndex],
+      message: MESSAGES[randomMessIndex],
       name: NAMES[randomNameIndex],
     };
   };
+  const similarComments = Array.from({length: getRandomPositiveInteger(1, 50)}, createComment);
   return {
-    id: randomID,
-    url: randomURL,
-    description: Desc[randomDescIndex],
+    id: randomId,
+    url: randomUrl,
+    description: DESCS[randomDescIndex],
     likes: randomLikes,
-    comments: createComments(),
+    comments: similarComments,
   };
 };
 
