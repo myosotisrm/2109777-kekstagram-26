539f56ae6001c105f5c05b711b79490d63ff4939
Mikhail <106912630+myosotisrm@users.noreply.github.com>
2022-07-22 17:32:20 +0300

    Отобразил фотографии других пользователей

---
+++ js/photo-creator.js
@@ -0,0 +1,21 @@
+
+//Шаблон для фотографии случайного пользователя
+const photosTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+//Собираем данные сюда, чтобы потом добавить Fragment к общим данным
+const photosFragment = document.createDocumentFragment();
+
+//Находим, куда добавить Fragment - в блок .pictures
+const photos = document.querySelector('.pictures');
+
+//Создаём DOM-элементы, соответствующие фотографиям
+const createPhotos = (photo) => {
+  photo.forEach(({url, likes, comments}) => {
+    const photoElement = photosTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
+    photosFragment.appendChild(pictureElement);
+  });
+  photos.appendChild(photosFragment);
+};
